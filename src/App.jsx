66dfd94e1af92052c497582f34efba93977c6308
Jesus-Iniesta <iniestavalverdejesus4@gmail.com>
2025-08-30 00:39:11 -0600

    feat: winner combos and winner Check

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,17 @@ const TURNS = {
   O: 'o'
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
@@ -22,12 +33,27 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 
 }
 
+const checkWinner = (boardToCheck) => {
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a]
+    }
+  }
+  return null
+}
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // null = no hay ganador, false = empate
 
   const updateBoard = (index) => {
-    if (board[index]) return
+    if (board[index] || winner) return
 
     const newBoard = [...board]
     newBoard[index] = turn
@@ -35,6 +61,14 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (!newBoard.includes(null)) {
+      setWinner(false) // Empate
+    }
+
+    
   }
 
   return (
